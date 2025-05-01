00037259a3a42d4bce0da199baf92fa7095659bc
ekikaja55 <rizkiarkant55@gmail.com>
2025-05-02 04:54:30 +0700

    coba coba

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import Link from 'next/link';
+export default function Dropdown() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="relative inline-block text-left">
+      <button onClick={() => setOpen(!open)}>explore ▼</button>
+      {open && (
+        <div className="absolute mt-2 w-40 bg-zinc-800 shadow-lg rounded">
+          <Link href="/" className="block px-4 py-2 hover:bg-gray-800 hover:text-amber-600">
+            wiki
+          </Link>
+          <a href="/about" className="block px-4 py-2 hover:bg-gray-800 hover:text-amber-600 ">
+            course
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
+/* eslint-disable */
+'use client';
 import Link from 'next/link';
 import styles from './styles/Navbar.module.css';
+import Dropdown from './Dropdown';
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+    <nav className="bg-zinc-700 p-4 flex justify-evenly items-center-safe gap-5 h-30 min-w-dvh ">
+      <Link href="/" className="text-4xl">
+        ByteSpace
+      </Link>
+      <div className="flex gap-4 ">
+        <Dropdown />
+        <Link href="/">news</Link>
+        <Link href="/">forums</Link>
+        <Link href="/">about us</Link>
+      </div>
+      <div className="">
+        <Link href="/">sign in</Link>
+      </div>
     </nav>
   );
 }
